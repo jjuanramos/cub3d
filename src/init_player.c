b892c11da3e84da2e249885dacbc92b3cfdb5b5f
jjuanramos <jjuanramos91@gmail.com>
2024-09-10 10:14:02 +0200

    added check

--- src/init_player.c
+++ src/init_player.c
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 10:45:05 by juramos           #+#    #+#             */
-/*   Updated: 2024/09/09 13:36:35 by juramos          ###   ########.fr       */
+/*   Updated: 2024/09/10 10:13:27 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,5 +105,5 @@ int	fill_player(t_mlx *mlx)
 		}
 		y++;
 	}
-	return (0);
+	return (times != 1);
 }
