c140569f40024f93ea1862fd79eaa8a91255cb58
jjuanramos <jjuanramos91@gmail.com>
2024-09-18 11:48:21 +0200

    added fences for edge values

--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 15:23:11 by juramos           #+#    #+#             */
-/*   Updated: 2024/09/18 11:18:34 by juramos          ###   ########.fr       */
+/*   Updated: 2024/09/18 11:45:28 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,11 +97,15 @@ static void	draw_wall(t_mlx *mlx, double t_pixel, double b_pixel,
 	texture = get_texture(mlx, normalize(mlx->ray->ray_ngl));
 	factor = (double)texture->height / wall_height;
 	x_offset = get_x_offset(texture, mlx);
+	if (x_offset >= texture->width)
+		x_offset = texture->width - 1;
 	y_offset = (t_pixel - (SCREENHEIGHT / 2) + (wall_height / 2)) * factor;
 	if (y_offset < 0)
 		y_offset = 0;
 	while (t_pixel < b_pixel)
 	{
+		if (y_offset >= texture->height)
+			y_offset = texture->height - 1;
 		text_pos = (int)y_offset * (texture->text->size_line / 4) \
 			+ (int)x_offset;
 		set_image_pixel(mlx->img, mlx->ray->index, t_pixel++,
@@ -122,6 +126,7 @@ static void	draw_floor_ceiling(t_mlx *mlx, double t_pixel,
 	while (i > 0)
 		set_image_pixel(mlx->img, mlx->ray->index, i--, 0x89CFF3FF);
 }
+
 // (TILE_SIZE / distance): scales wall height by distance;
 // (SCREENWIDTH / 2) / tan(fov/2): finds projection plane distance for FOV.
 void	render(t_mlx *mlx, int i)
